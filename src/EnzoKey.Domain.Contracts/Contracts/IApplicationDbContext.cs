e75cb8c47ad1a61aada485da984303dfc23f357b
Carlos Alberto de Lima Campos <enzojb@msn.com>
2025-07-24 20:58:17 -0300

    feat: Inclusão da Interface de DbContext

---
+++ src/EnzoKey.Domain.Contracts/Contracts/IApplicationDbContext.cs
@@ -0,0 +1,34 @@
+﻿using EnzoKey.Domain.Contracts.Model.DTO;
+using Microsoft.EntityFrameworkCore;
+
+namespace EnzoKey.Domain.Contracts.Contracts;
+
+public interface IApplicationDbContext
+{
+    /// <summary>
+    /// Dbset para a entidade <see cref="Cliente"/>.
+    /// </summary>
+    DbSet<Cliente> Clientes { get; set; }
+
+    /// <summary>
+    /// Dbset para a entidade <see cref="Produto"/>.
+    /// </summary>
+    DbSet<Produto> Produtos { get; set; }
+
+    /// <summary>
+    /// Dbset para a entidade <see cref="Licenca"/>.
+    /// </summary>
+    DbSet<Licenca> Licencas { get; set; }
+
+    /// <summary>
+    /// Dbset para a entidade <see cref="UsuarioAdmin"/>.
+    /// </summary>
+    DbSet<UsuarioAdmin> UsuariosAdmin { get; set; }
+
+    /// <summary>
+    /// Método para salvar as alterações no contexto do banco de dados.
+    /// </summary>
+    /// <param name="cancellationToken">Token de cancelamento</param>
+    /// <returns>Retorna uma <see cref="Task Int"/></returns>
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+}
